9acd65bd9735a1bed56e9230b2c903985773c41d
Canjia Huang <60030853+Canjia-Huang@users.noreply.github.com>
2026-08-18 22:46:27 +0800

    WIP: add GeoBoxApplication and main entry point for geobox component

---
+++ src/geolio/geobox/application.cpp
@@ -0,0 +1,12 @@
+//
+// Created by huangcanjia <huangcanjia0214@gmail.com> on 2026/8/18.
+// Copyright (c) 2026 Graphics@XMU (https://graphics.xmu.edu.cn). All rights reserved.
+//
+#include "application.h"
+
+namespace geolio
+{
+    GeoBoxApplication::GeoBoxApplication(
+        ) : SimpleMeshApplication("GeoBox")
+    {}
+}
---
+++ src/geolio/geobox/application.h
@@ -0,0 +1,21 @@
+//
+// Created by huangcanjia <huangcanjia0214@gmail.com> on 2026/8/18.
+// Copyright (c) 2026 Graphics@XMU (https://graphics.xmu.edu.cn). All rights reserved.
+//
+#ifndef GEOLIO_APPLICATION_H
+#define GEOLIO_APPLICATION_H
+
+#include <geogram_gfx/gui/simple_mesh_application.h>
+
+namespace geolio
+{
+    class GeoBoxApplication : public GEO::SimpleMeshApplication {
+    public:
+        GeoBoxApplication();
+
+    private:
+
+    };
+}
+
+#endif //GEOLIO_APPLICATION_H
