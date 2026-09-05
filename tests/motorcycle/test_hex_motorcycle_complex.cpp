093ab49fae22a5c6ccbc72ac8ddb46bd7a9804ae
Canjia Huang <60030853+Canjia-Huang@users.noreply.github.com>
2026-09-05 22:47:47 +0800

    feat: enhance save_results method to include new coarse mesh creation and update block labeling

--- tests/motorcycle/test_hex_motorcycle_complex.cpp
+++ tests/motorcycle/test_hex_motorcycle_complex.cpp
@@ -19,50 +19,53 @@ namespace geolio::test
         }
 
         void save_results() {
-            {
-                GEO::Attribute<GEO::index_t> hex_c_block(hex_mesh.cells.attributes(), "block");
-                MC->label_blocks(hex_c_block);
+            GEO::Attribute<GEO::index_t> hex_c_block(hex_mesh.cells.attributes(), "block");
+            MC->label_blocks(hex_c_block);
 
-                std::vector<std::pair<GEO::index_t, GEO::index_t>> cf_to_create;
-                std::vector<bool> processed_cf(8*hex_mesh.cells.nb(), false);
-                for (const auto& c : hex_mesh.cells) {
-                    for (GEO::index_t lf = 0; lf < 6; ++lf) {
-                        if (processed_cf[8*c+lf])
-                            continue;
+            std::vector<std::pair<GEO::index_t, GEO::index_t>> cf_to_create;
+            std::vector<bool> processed_cf(8*hex_mesh.cells.nb(), false);
+            for (const auto& c : hex_mesh.cells) {
+                for (GEO::index_t lf = 0; lf < 6; ++lf) {
+                    if (processed_cf[8*c+lf])
+                        continue;
 
-                        if (const auto nc = hex_mesh.cells.adjacent(c, lf);
-                            nc == GEO::NO_CELL || hex_c_block[c] != hex_c_block[nc]
-                            ) {
-                            cf_to_create.emplace_back(c, lf);
+                    if (const auto nc = hex_mesh.cells.adjacent(c, lf);
+                        nc == GEO::NO_CELL || hex_c_block[c] != hex_c_block[nc]
+                        ) {
+                        cf_to_create.emplace_back(c, lf);
 
-                            processed_cf[8*c+lf] = true;
-                            if (nc != GEO::NO_CELL) {
-                                const auto nlf = find_hex_facet(
-                                    hex_mesh,
-                                    nc,
-                                    hex_mesh.cells.facet_vertex(c, lf, 2),
-                                    hex_mesh.cells.facet_vertex(c, lf, 1),
-                                    hex_mesh.cells.facet_vertex(c, lf, 0));
-                                assert(nlf != GEO::NO_INDEX);
-                                processed_cf[8*nc+nlf] = true;
-                            }
+                        processed_cf[8*c+lf] = true;
+                        if (nc != GEO::NO_CELL) {
+                            const auto nlf = find_hex_facet(
+                                hex_mesh,
+                                nc,
+                                hex_mesh.cells.facet_vertex(c, lf, 2),
+                                hex_mesh.cells.facet_vertex(c, lf, 1),
+                                hex_mesh.cells.facet_vertex(c, lf, 0));
+                            assert(nlf != GEO::NO_INDEX);
+                            processed_cf[8*nc+nlf] = true;
                         }
                     }
                 }
-                GEO::index_t new_f = hex_mesh.facets.create_quads(cf_to_create.size());
-                for (const auto& [c, lf] : cf_to_create) {
-                    for (GEO::index_t lv = 0; lv < 4; ++lv)
-                        hex_mesh.facets.set_vertex(new_f, lv, hex_mesh.cells.facet_vertex(c, lf, lv));
-                    ++new_f;
-                }
+            }
+            GEO::index_t new_f = hex_mesh.facets.create_quads(cf_to_create.size());
+            for (const auto& [c, lf] : cf_to_create) {
+                for (GEO::index_t lv = 0; lv < 4; ++lv)
+                    hex_mesh.facets.set_vertex(new_f, lv, hex_mesh.cells.facet_vertex(c, lf, lv));
+                ++new_f;
+            }
 
-                hex_mesh.save(get_current_test_name()+"_block.geogram");
-            }
-            {
-                GEO::Mesh coarse_mesh;
-                MC->create_coarse_mesh(coarse_mesh);
-                coarse_mesh.save(get_current_test_name()+"_coarse.geogram");
-            }
+            GEO::Mesh coarse_mesh;
+            std::vector<GEO::index_t> old_cf_to_new_cf;
+            MC->create_coarse_mesh(coarse_mesh, &old_cf_to_new_cf);
+            ASSERT_EQ(old_cf_to_new_cf.size(), 8*hex_mesh.cells.nb());
+
+            GEO::Attribute<GEO::index_t> hex_mesh_cf_to_new_cf(hex_mesh.cell_facets.attributes(), "new_cf");
+            for (const auto& cf : hex_mesh.cell_facets)
+                hex_mesh_cf_to_new_cf[cf] = old_cf_to_new_cf[cf];
+
+            hex_mesh.save(get_current_test_name()+"_block.geogram");
+            coarse_mesh.save(get_current_test_name()+"_coarse.geogram");
         }
 
         GEO::Mesh hex_mesh;
@@ -71,6 +74,11 @@ namespace geolio::test
 
     TEST_F(HexMotorcycleComplexTest, base_complex) {
         MC->compute(HexMotorCycleComplex::BASE_COMPLEX);
+        save_results();
+    }
+
+    TEST_F(HexMotorcycleComplexTest, motorcycle_complex) {
+        MC->compute(HexMotorCycleComplex::MOTORCYCLE_COMPLEX);
         save_results();
     }
 }
